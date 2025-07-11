a1701dc64db929edce99f0935f7dd133139d9404
KarUladz <u.karnialiuk@gmail.com>
2025-07-11 22:59:29 +0300

    feat: add rectangle task

--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -1,7 +1,27 @@
 package school.mjc.stage0.loops.task5;
 
 public class Rectangle {
-    public void printRectangleFrom8s(int length, int height){
+    public void printRectangleFrom8s(int length, int height) {
+        if (length <= 0 || height <= 0) {
+            return;
+        }
+
+        for (int i = 0; i < height; i++) {
+            StringBuilder builder = new StringBuilder();
+
+            if (i == 0 || i == height - 1) {
+                builder.append("8".repeat(length));
+            } else {
+                for (int j = 0; j < length; j++) {
+                    if (j == 0 || j == length - 1) {
+                        builder.append(8);
+                    } else {
+                        builder.append(" ");
+                    }
+                }
+            }
+            System.out.println(builder.toString());
+        }
 
     }
 }
