99f167caad33b62e82a8de42fb54efc3af248350
KarUladz <u.karnialiuk@gmail.com>
2025-07-11 21:45:55 +0300

    feat: add cross task

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,24 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
+        int eightLength = 8;
 
+        int middleNum = (int) Math.ceil(sideLength / (double) 2);
+
+        for (int i = 1; i <= sideLength; i++) {
+            StringBuilder crossString = new StringBuilder();
+            if (i == middleNum) {
+                crossString.append(String.valueOf(eightLength).repeat(sideLength));
+            } else {
+                for (int j = 1; j <= sideLength; j++) {
+                    if (j == middleNum) {
+                        crossString.append(eightLength);
+                    } else {
+                        crossString.append(" ");
+                    }
+                }
+            }
+            System.out.println(crossString);
+        }
     }
 }
