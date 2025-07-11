999c2d7398a57e9f823bcb35840b634daebb2f91
KarUladz <u.karnialiuk@gmail.com>
2025-07-11 21:23:49 +0300

    feat: add square task

--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -1,7 +1,29 @@
 package school.mjc.stage0.loops.task5;
 
 public class Square {
-    public void printSquareFrom8s(int sideLength){
 
+    public void printSquareFrom8s(int sideLength){
+        int eight = 8;
+
+        StringBuilder mainString = new StringBuilder();
+        StringBuilder otherString = new StringBuilder();
+
+        mainString.append(String.valueOf(eight).repeat(sideLength));
+
+        for (int i = 0; i < sideLength; i++) {
+            if (i == 0 || i == sideLength - 1) {
+                otherString.append(eight);
+            } else {
+                otherString.append(" ");
+            }
+        }
+
+        for (int i = 0; i < sideLength; i++) {
+            if (i == 0 || i == sideLength - 1) {
+                System.out.println(mainString);
+            } else {
+                System.out.println(otherString);
+            }
+        }
     }
 }
