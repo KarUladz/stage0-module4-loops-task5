0f8acb512ecb2aeec18bf0251565f7757c5d2050
KarUladz <u.karnialiuk@gmail.com>
2025-07-11 22:00:45 +0300

    fix: square task

--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -2,28 +2,25 @@ package school.mjc.stage0.loops.task5;
 
 public class Square {
 
-    public void printSquareFrom8s(int sideLength){
-        int eight = 8;
-
-        StringBuilder mainString = new StringBuilder();
-        StringBuilder otherString = new StringBuilder();
-
-        mainString.append(String.valueOf(eight).repeat(sideLength));
-
-        for (int i = 0; i < sideLength; i++) {
-            if (i == 0 || i == sideLength - 1) {
-                otherString.append(eight);
-            } else {
-                otherString.append(" ");
-            }
+    public void printSquareFrom8s(int sideLength) {
+        if (sideLength <= 0) {
+            return;
         }
 
         for (int i = 0; i < sideLength; i++) {
+            StringBuilder builder = new StringBuilder();
             if (i == 0 || i == sideLength - 1) {
-                System.out.println(mainString);
+                builder.append(String.valueOf(8).repeat(sideLength));
             } else {
-                System.out.println(otherString);
+                for (int j = 0; j < sideLength; j++) {
+                    if (j == 0 || j == sideLength - 1) {
+                        builder.append(8);
+                    } else {
+                        builder.append(" ");
+                    }
+                }
             }
+            System.out.println(builder.toString());
         }
     }
 }
