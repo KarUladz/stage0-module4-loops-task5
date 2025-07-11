0e1b296de3493c9960aa0dac87988b71dd7cff19
KarUladz <u.karnialiuk@gmail.com>
2025-07-11 21:53:42 +0300

    feat: add triangle task

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
+       StringBuilder builder = new StringBuilder();
+       builder.append(String.valueOf(" ").repeat(cathetusLength));
+
+       for (int i = 0; i < cathetusLength; i++) {
+           builder.setCharAt(i, '8');
+           System.out.println(builder.toString());
+
+       }
 
     }
 }
